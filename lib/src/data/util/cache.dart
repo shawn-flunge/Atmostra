272d88ee1997297f1d2dfe1de5e75d081b04d83d
shawn-flunge <echun1234@gmail.com>
2023-04-04 21:48:29 +0900

    modify field name 'name' -> 'identifier'

--- lib/src/data/util/cache.dart
+++ lib/src/data/util/cache.dart
@@ -3,30 +3,30 @@
 class Cache<T>{
   const Cache({
     required this.requestAt,
-    required this.path,
+    required this.identifier,
     required this.data,
   });
 
-  final int requestAt;
-  final String path;
+  final DateTime requestAt;
+  final String identifier;
   final T data;
 
-  operator <(int mill){
-    return mill < requestAt + 5;
+  operator <(DateTime date){
+    return date.millisecondsSinceEpoch < requestAt.millisecondsSinceEpoch + 300000;
   }
 
   @override
   bool operator ==(Object other) {
     final o = other as Cache;
-    return path == o.path && this < o.requestAt;
+    return identifier == o.identifier && this < o.requestAt;
   }
 }
 
 extension CacheListExtension on List<Cache>{
 
-  (bool, int) hasData(String path){
+  (bool, int) hasData(String identifier){
 
-    final index = indexWhere((element) => element.path == path);
+    final index = indexWhere((element) => element.identifier == identifier);
     if(index == -1) return (false, -1);
 
     return (true, index);
