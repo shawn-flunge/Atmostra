0ec6f003287171f8cc1605336ba9ef7c02c69467
shawn-flunge <echun1234@gmail.com>
2023-04-04 21:51:08 +0900

    create planet type, add method to call weather api

--- lib/src/state/planet.dart
+++ lib/src/state/planet.dart
@@ -1,35 +1,49 @@
 
 
+import 'package:atmostra/src/data/dto/weather_dto.dart';
+import 'package:atmostra/src/data/remote/open_weather_api.dart';
 import 'package:flutter/material.dart';
 
 class Planets extends ChangeNotifier{
 
+  // todo : load user setting
   Planets(){
-    planets.add(
-      PlanetDto(name: 'E-KOREA-SEOUL', dateTime: DateTime.now())
-    );
-
-    selected = planets.first;
-    print('${planets.first.name}');
+    _weatherApi = OpenWeatherApi();
   }
 
+  late OpenWeatherApi _weatherApi;
 
   PlanetDto? selected;
   final List<PlanetDto> planets = [];
 
+  getWeatherByName(String name) async{
+    final weather = await _weatherApi.getWeatherByName(name: name);
+    if(weather == null) return;
+    final planet = PlanetDto(
+      name: weather.name,
+      weather: weather,
+    );
+    planets.add(planet);
+    notifyListeners();
+  }
 
 }
 
 
-
+enum PlanetType{
+  earth,
+  custom,
+}
 
 class PlanetDto{
 
   const PlanetDto({
     required this.name,
-    required this.dateTime
+    this.type = PlanetType.earth,
+    required this.weather
   });
 
   final String name;
-  final DateTime dateTime;
+  final PlanetType type;
+  final WeatherDto weather;
 }
